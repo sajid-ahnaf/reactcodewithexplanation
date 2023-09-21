cbc8da994b33d8b448b37042d76a3297d90ad7c3
Sajid Hasan <85543838+sajid-ahnaf@users.noreply.github.com>
2023-09-22 01:35:39 +0600

    Update 34-add-event-listeners.jsx

--- React/34-add-event-listeners.jsx
+++ React/34-add-event-listeners.jsx
@@ -1,35 +1,55 @@
+// Define a class-based component called MyComponent.
 class MyComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        message: ''
-      };
-      this.handleEnter = this.handleEnter.bind(this);
-      this.handleKeyPress = this.handleKeyPress.bind(this);
+  // Constructor for MyComponent that takes props as a parameter.
+  constructor(props) {
+    // Call the constructor of the parent class (React.Component) and pass the props.
+    super(props);
+
+    // Initialize the component's state with a property called 'message' set to an empty string.
+    this.state = {
+      message: ''
+    };
+
+    // Bind the 'handleEnter' and 'handleKeyPress' methods to the current instance of the component.
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  // Define a method called 'handleEnter'.
+  handleEnter() {
+    // Update the component's state by appending a message when the enter key is pressed.
+    this.setState({
+      message: this.state.message + 'You pressed the enter key! '
+    });
+  }
+
+  // Define a method called 'handleKeyPress' that takes an 'event' as a parameter.
+  handleKeyPress(event) {
+    // Check if the key code of the event is 13 (which corresponds to the enter key).
+    if (event.keyCode === 13) {
+      // If the enter key is pressed, call the 'handleEnter' method.
+      this.handleEnter();
     }
-    // change code below this line
-    componentDidMount() {
-      document.addEventListener("keydown",this.handleKeyPress);
-    }
-    componentWillUnmount() {
-      document.removeEventListener("keydown",this.handleKeyPress);
-    }
-    // change code above this line
-    handleEnter() {
-      this.setState({
-        message: this.state.message + 'You pressed the enter key! '
-      });
-    }
-    handleKeyPress(event) {
-      if (event.keyCode === 13) {
-        this.handleEnter();
-      }
-    }
-    render() {
-      return (
-        <div>
-          <h1>{this.state.message}</h1>
-        </div>
-      );
-    }
-  };
+  }
+
+  // Lifecycle method: componentDidMount is called after the component is added to the DOM.
+  componentDidMount() {
+    // Add an event listener to the document to listen for keydown events and call 'handleKeyPress' when the event occurs.
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  // Lifecycle method: componentWillUnmount is called before the component is removed from the DOM.
+  componentWillUnmount() {
+    // Remove the previously added event listener to prevent memory leaks when the component is unmounted.
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  // Define the render method for MyComponent.
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+}
