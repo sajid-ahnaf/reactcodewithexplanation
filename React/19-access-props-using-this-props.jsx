a1bd91fea88c1b2cff55c31edf1fa2a5d80a55ab
Sajid Hasan <85543838+sajid-ahnaf@users.noreply.github.com>
2023-09-22 01:29:16 +0600

    Update 19-access-props-using-this-props.jsx

--- React/19-access-props-using-this-props.jsx
+++ React/19-access-props-using-this-props.jsx
@@ -1,31 +1,43 @@
+// Define a class-based component called ReturnTempPassword.
 class ReturnTempPassword extends React.Component {
-    constructor(props) {
-      super(props);
-  
-    }
-    render() {
-      return (
-          <div>
-              
-              <p>Your temporary password is: <strong>{this.props.tempPassword}</strong></p>
-          </div>
-      );
-    }
-  };
-  
-  class ResetPassword extends React.Component {
-    constructor(props) {
-      super(props);
-  
-    }
-    render() {
-      return (
-          <div>
-            <h2>Reset Password</h2>
-            <h3>We've generated a new temporary password for you.</h3>
-            <h3>Please reset this password from your account settings ASAP.</h3>
-           <ReturnTempPassword tempPassword={'Elon Rocks'} />
-          </div>
-      );
-    }
-  };
+  // Constructor for ReturnTempPassword that takes props as a parameter.
+  constructor(props) {
+    // Call the constructor of the parent class (React.Component) and pass the props.
+    super(props);
+  }
+
+  // Define the render method for ReturnTempPassword.
+  render() {
+    // Return JSX code wrapped in parentheses.
+    return (
+      // JSX code representing a <div> element with a <p> element inside.
+      <div>
+        <p>Your temporary password is: <strong>{this.props.tempPassword}</strong></p>
+      </div>
+    );
+  }
+};
+
+// Define another class-based component called ResetPassword.
+class ResetPassword extends React.Component {
+  // Constructor for ResetPassword that takes props as a parameter.
+  constructor(props) {
+    // Call the constructor of the parent class (React.Component) and pass the props.
+    super(props);
+  }
+
+  // Define the render method for ResetPassword.
+  render() {
+    // Return JSX code wrapped in parentheses.
+    return (
+      // JSX code representing a <div> element with multiple <h2> and <h3> elements, and a ReturnTempPassword component.
+      <div>
+        <h2>Reset Password</h2>
+        <h3>We've generated a new temporary password for you.</h3>
+        <h3>Please reset this password from your account settings ASAP.</h3>
+        {/* Render the ReturnTempPassword component here and pass it a temporary password as a prop. */}
+        <ReturnTempPassword tempPassword={'Elon Rocks'} />
+      </div>
+    );
+  }
+};
