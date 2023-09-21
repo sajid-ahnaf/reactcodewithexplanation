906374ba464dd73f887e914dc306eaab5de882eb
Sajid Hasan <85543838+sajid-ahnaf@users.noreply.github.com>
2023-09-22 01:25:24 +0600

    Update 09-create-a-component-with-composition.jsx

--- React/09-create-a-component-with-composition.jsx
+++ React/09-create-a-component-with-composition.jsx
@@ -1,24 +1,33 @@
+// Define a constant variable called ChildComponent, which is a functional component.
 const ChildComponent = () => {
+  // Return JSX (JavaScript XML) code wrapped in parentheses.
+  return (
+    // JSX code representing a <div> element with a <p> element inside.
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+// Define a class-based component called ParentComponent that extends React.Component.
+class ParentComponent extends React.Component {
+  // Constructor for ParentComponent that takes props as a parameter.
+  constructor(props) {
+    // Call the constructor of the parent class (React.Component) and pass the props.
+    super(props);
+  }
+  // Define the render method for ParentComponent.
+  render() {
+    // Return JSX code wrapped in parentheses.
     return (
+      // JSX code representing a <div> element with an <h1> element and ChildComponent inside.
       <div>
-        <p>I am the child</p>
+        <h1>I am the parent</h1>
+        { /* change code below this line */ }
+        {/* Render the ChildComponent component here */}
+        <ChildComponent />
+        { /* change code above this line */ }
       </div>
     );
-  };
-  
-  class ParentComponent extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-      return (
-        <div>
-          <h1>I am the parent</h1>
-          { /* change code below this line */ }
-          <ChildComponent />
-  
-          { /* change code above this line */ }
-        </div>
-      );
-    }
-  };
+  }
+};
