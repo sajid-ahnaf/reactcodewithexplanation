3c9d6b5b5e4006bbd533453939fbf5d164f69322
Sajid Hasan <85543838+sajid-ahnaf@users.noreply.github.com>
2023-09-22 08:24:05 +0600

    Update 06-learn-about-self-closing-jsx-tags.jsx

--- React/06-learn-about-self-closing-jsx-tags.jsx
+++ React/06-learn-about-self-closing-jsx-tags.jsx
@@ -5,4 +5,12 @@ const JSX = (
     <hr />
   </div>
 );
-/*A self-closing tag, also known as an empty or void element, is a type of HTML or XML element that doesn't have a closing tag. Instead, it is closed immediately after its opening tag with a forward slash before the closing angle bracket. Self-closing tags are used for elements that don't contain any content between open and close tags. Common examples include the `<img>`, `<br>`, and `<input>` tags in HTML. They make code more readable and are essential for compatibility with XML and XHTML. In HTML5, you can omit the closing slash, but including it is recommended for consistency and compatibility.*/
+/*A self-closing tag, also known as an empty or void element, 
+is a type of HTML or XML element that doesn't have a closing tag. 
+Instead, it is closed immediately after its opening tag with a 
+forward slash before the closing angle bracket. Self-closing tags 
+are used for elements that don't contain any content between open 
+and close tags. Common examples include the `<img>`, `<br>`, and 
+`<input>` tags in HTML. They make code more readable and are essential
+for compatibility with XML and XHTML. In HTML5, you can omit the closing
+slash, but including it is recommended for consistency and compatibility.*/
